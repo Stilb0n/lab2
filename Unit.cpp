a32082326c6968fe6257b88922739ec21f71cbf0
Stilbon <stilbon01@gmail.com>
2023-06-20 15:15:30 +0700

    начал реализовывать паттерн абстрактной фабрики.
    Объявил прототипы классов из паттерна: интерфейсы для генерирования методов и классов. Фабрики для конкретных языков и абстрактная фабрика. Начал реализовывать фабрику генерации кода на языке C#.

--- Unit.cpp
+++ Unit.cpp
@@ -2,6 +2,7 @@
 #include <memory>
 #include <vector>
 
+
 class Unit {
   public: using Flags = unsigned int;
   public: virtual~Unit() =
@@ -136,3 +137,138 @@ std::string generateProgram2() {
   myClass.add(method, ClassUnit::PROTECTED);
   return myClass.compile();
 }
+
+
+
+class IMethodUnit: public Unit {};
+
+ class IClassUnit: public Unit {  };
+
+
+class CsharpMethodUnit: public IMethodUnit {
+public: enum Modifier {
+  STATIC = 1,
+    CONST = 1 << 1,
+    VIRTUAL = 1 << 2
+};
+public: CsharpMethodUnit(const std::string & name,
+    const std::string & returnType, Flags
+  flags): m_name(name),
+m_returnType(returnType),
+m_flags(flags) {}
+void add(const std::shared_ptr < Unit > & unit, Flags /* flags */ = 0) {
+  m_body.push_back(unit);
+}
+std::string compile(unsigned int level = 0) const {
+  std::string result = generateShift(level);
+  if (m_flags & STATIC) {
+    result += "static ";
+  } else if (m_flags & VIRTUAL) {
+    result += "virtual ";
+  }
+  result += m_returnType + " ";
+  result += m_name + "()";
+  if (m_flags & CONST) {
+    result += " const";
+  }
+  result += " {\n";
+  for (const auto & b: m_body) {
+    result += b -> compile(level + 1);
+  }
+  result += generateShift(level) + "}\n";
+  return result;
+}
+private: std::string m_name;
+std::string m_returnType;
+Flags m_flags;
+std::vector < std::shared_ptr < Unit > > m_body;};
+class CsharpClassUnit: public IClassUnit {
+public: enum AccessModifier {
+  PUBLIC,
+  PROTECTED,
+  PRIVATE
+};
+static
+const std::vector < std::string > ACCESS_MODIFIERS;
+public: explicit CsharpClassUnit(const std::string & name): m_name(name) {
+  m_fields.resize(ACCESS_MODIFIERS.size());
+}
+void add (const std::shared_ptr < Unit > & unit, Flags flags) override{
+
+  int accessModifier = PRIVATE;
+
+  if (flags < ACCESS_MODIFIERS.size()) {
+    accessModifier = flags;
+  }
+  m_fields[accessModifier].push_back(unit);
+}
+std::string  compile(unsigned int level = 0)  const override{
+  std::string result = generateShift(level) + "class " + m_name + " {\n";
+
+  for (size_t i = 0; i < ACCESS_MODIFIERS.size(); ++i) {
+    if (m_fields[i].empty()) {
+      continue;
+    }
+    result += ACCESS_MODIFIERS[i] + ":\n";
+    for (const auto & f: m_fields[i]) {
+      result += f -> compile(level + 1);
+    }
+    result += "\n";
+  }
+  result += generateShift(level) + "};\n";
+  return result;
+}
+private: std::string m_name;
+using Fields = std::vector < std::shared_ptr < Unit > > ;
+std::vector < Fields > m_fields;
+};
+class JavaMethodUnit: IMethodUnit {};
+//class JavaClassUnit: IClassUnit {};
+class CMethodUnit: IMethodUnit {};
+//class CClassUnit: IClassUnit {};
+
+
+
+
+
+
+
+class Fabric{
+public:
+    virtual IClassUnit* createClass()=0;
+    virtual IMethodUnit* createMethod()=0;
+};
+
+class JavaFabric: public Fabric {
+
+};
+class CFabric: public Fabric {
+
+};
+
+
+
+
+
+class CsharpFabric: public Fabric {
+    IClassUnit* createClass()override{
+    return new CsharpClassUnit("kek");
+    }
+    IMethodUnit* createMethod() override{
+        return new CsharpMethodUnit("foo","void",CsharpMethodUnit::STATIC);
+        }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <QCoreApplication>
 #include<iostream>
 #include "Unit.cpp"
-#include "lol.cpp"
 int main(int argc, char *argv[])
 {
 
