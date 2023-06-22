01da1f55acac243b62f1f0375e6f866fabc32084
Stilbon <stilbon01@gmail.com>
2023-06-22 15:30:57 +0700

    Заполнил классы по созданию юнитов.
    Пока что реализация идентичная созданию юнитов на С++. Буду понемногу менять эти классы.

--- Unit.cpp
+++ Unit.cpp
@@ -23,90 +23,10 @@ class Unit {
   }
 };
 
-class ClassUnit: public Unit {
-  public: enum AccessModifier {
-    PUBLIC,
-    PROTECTED,
-    PRIVATE
-  };
-  static
-  const std::vector < std::string > ACCESS_MODIFIERS;
-  public: explicit ClassUnit(const std::string & name): m_name(name) {
-    m_fields.resize(ACCESS_MODIFIERS.size());
-  }
-  void add(const std::shared_ptr < Unit > & unit, Flags flags) {
 
-    int accessModifier = PRIVATE;
 
-    if (flags < ACCESS_MODIFIERS.size()) {
-      accessModifier = flags;
-    }
-    m_fields[accessModifier].push_back(unit);
-  }
-  std::string compile(unsigned int level = 0) const {
-    std::string result = generateShift(level) + "class " + m_name + " {\n";
 
-    for (size_t i = 0; i < ACCESS_MODIFIERS.size(); ++i) {
-      if (m_fields[i].empty()) {
-        continue;
-      }
-      result += ACCESS_MODIFIERS[i] + ":\n";
-      for (const auto & f: m_fields[i]) {
-        result += f -> compile(level + 1);
-      }
-      result += "\n";
-    }
-    result += generateShift(level) + "};\n";
-    return result;
-  }
-  private: std::string m_name;
-  using Fields = std::vector < std::shared_ptr < Unit > > ;
-  std::vector < Fields > m_fields;
-};
-const std::vector < std::string > ClassUnit::ACCESS_MODIFIERS = {
-  "public",
-  "protected",
-  "private"
-};
 
-class MethodUnit: public Unit {
-  public: enum Modifier {
-    STATIC = 1,
-      CONST = 1 << 1,
-      VIRTUAL = 1 << 2
-  };
-  public: MethodUnit(const std::string & name,
-      const std::string & returnType, Flags
-    flags): m_name(name),
-  m_returnType(returnType),
-  m_flags(flags) {}
-  void add(const std::shared_ptr < Unit > & unit, Flags /* flags */ = 0) {
-    m_body.push_back(unit);
-  }
-  std::string compile(unsigned int level = 0) const {
-    std::string result = generateShift(level);
-    if (m_flags & STATIC) {
-      result += "static ";
-    } else if (m_flags & VIRTUAL) {
-      result += "virtual ";
-    }
-    result += m_returnType + " ";
-    result += m_name + "()";
-    if (m_flags & CONST) {
-      result += " const";
-    }
-    result += " {\n";
-    for (const auto & b: m_body) {
-      result += b -> compile(level + 1);
-    }
-    result += generateShift(level) + "}\n";
-    return result;
-  }
-  private: std::string m_name;
-  std::string m_returnType;
-  Flags m_flags;
-  std::vector < std::shared_ptr < Unit > > m_body;
-};
 
 class PrintOperatorUnit: public Unit {
   public: explicit PrintOperatorUnit(const std::string & text): m_text(text) {}
@@ -116,33 +36,33 @@ class PrintOperatorUnit: public Unit {
   private: std::string m_text;
 };
 
-std::string generateProgram2() {
-  ClassUnit myClass("MyClass");
-  myClass.add(
-    std::make_shared < MethodUnit > ("testFunc1", "void", 0),
-    ClassUnit::PUBLIC
-  );
-  myClass.add(
-    std::make_shared < MethodUnit > ("testFunc2", "void", MethodUnit::STATIC),
-    ClassUnit::PRIVATE
-  );
-  myClass.add(
-    std::make_shared < MethodUnit > ("testFunc3", "void", MethodUnit::VIRTUAL |
-      MethodUnit::CONST),
-    ClassUnit::PUBLIC
-  );
-  auto method = std::make_shared < MethodUnit > ("testFunc4", "void",
-    MethodUnit::STATIC);
-  method -> add(std::make_shared < PrintOperatorUnit > ( "(Hello, world!\n)"));
-  myClass.add(method, ClassUnit::PROTECTED);
-  return myClass.compile();
-}
+//std::string generateProgram2() {
+//  ClassUnit myClass("MyClass");
+//  myClass.add(
+//    std::make_shared < MethodUnit > ("testFunc1", "void", 0),
+//    ClassUnit::PUBLIC
+//  );
+//  myClass.add(
+//    std::make_shared < MethodUnit > ("testFunc2", "void", MethodUnit::STATIC),
+//    ClassUnit::PRIVATE
+//  );
+//  myClass.add(
+//    std::make_shared < MethodUnit > ("testFunc3", "void", MethodUnit::VIRTUAL |
+//      MethodUnit::CONST),
+//    ClassUnit::PUBLIC
+//  );
+//  auto method = std::make_shared < MethodUnit > ("testFunc4", "void",
+//    MethodUnit::STATIC);
+//  method -> add(std::make_shared < PrintOperatorUnit > ( "(Hello, world!\n)"));
+//  myClass.add(method, ClassUnit::PROTECTED);
+//  return myClass.compile();
+//}
 
 
 
 class IMethodUnit: public Unit {};
 
- class IClassUnit: public Unit {  };
+ class IClassUnit: public Unit {};
 
 
 class CsharpMethodUnit: public IMethodUnit {
@@ -222,14 +142,167 @@ private: std::string m_name;
 using Fields = std::vector < std::shared_ptr < Unit > > ;
 std::vector < Fields > m_fields;
 };
-class JavaMethodUnit: IMethodUnit {};
-//class JavaClassUnit: IClassUnit {};
-class CMethodUnit: IMethodUnit {};
-//class CClassUnit: IClassUnit {};
+const std::vector< std::string > CsharpClassUnit::ACCESS_MODIFIERS = { "public",
+"protected", "private" };
 
+class JavaMethodUnit:public IMethodUnit { public: enum Modifier {
+        STATIC = 1,
+          CONST = 1 << 1,
+          VIRTUAL = 1 << 2
+      };
+      public: JavaMethodUnit(const std::string & name,
+          const std::string & returnType, Flags
+        flags): m_name(name),
+      m_returnType(returnType),
+      m_flags(flags) {}
+      void add(const std::shared_ptr < Unit > & unit, Flags /* flags */ = 0) {
+        m_body.push_back(unit);
+      }
+      std::string compile(unsigned int level = 0) const {
+        std::string result = generateShift(level);
+        if (m_flags & STATIC) {
+          result += "static ";
+        } else if (m_flags & VIRTUAL) {
+          result += "virtual ";
+        }
+        result += m_returnType + " ";
+        result += m_name + "()";
+        if (m_flags & CONST) {
+          result += " const";
+        }
+        result += " {\n";
+        for (const auto & b: m_body) {
+          result += b -> compile(level + 1);
+        }
+        result += generateShift(level) + "}\n";
+        return result;
+      }
+      private: std::string m_name;
+      std::string m_returnType;
+      Flags m_flags;
+      std::vector < std::shared_ptr < Unit > > m_body;
+    };
+class JavaClassUnit:public IClassUnit {
+public: enum AccessModifier {
+    PUBLIC,
+    PROTECTED,
+    PRIVATE
+  };
+  static
+  const std::vector < std::string > ACCESS_MODIFIERS;
+  public: explicit JavaClassUnit(const std::string & name): m_name(name) {
+    m_fields.resize(ACCESS_MODIFIERS.size());
+  }
+  void add (const std::shared_ptr < Unit > & unit, Flags flags) override{
 
+    int accessModifier = PRIVATE;
 
+    if (flags < ACCESS_MODIFIERS.size()) {
+      accessModifier = flags;
+    }
+    m_fields[accessModifier].push_back(unit);
+  }
+  std::string  compile(unsigned int level = 0)  const override{
+    std::string result = generateShift(level) + "class " + m_name + " {\n";
 
+    for (size_t i = 0; i < ACCESS_MODIFIERS.size(); ++i) {
+      if (m_fields[i].empty()) {
+        continue;
+      }
+      result += ACCESS_MODIFIERS[i] + ":\n";
+      for (const auto & f: m_fields[i]) {
+        result += f -> compile(level + 1);
+      }
+      result += "\n";
+    }
+    result += generateShift(level) + "};\n";
+    return result;};
+private: std::string m_name;
+using Fields = std::vector < std::shared_ptr < Unit > > ;
+std::vector < Fields > m_fields;
+};
+const std::vector< std::string > JavaClassUnit::ACCESS_MODIFIERS = { "public",
+"protected", "private" };
+
+class CMethodUnit:public IMethodUnit {
+public: enum Modifier {
+  STATIC = 1,
+    CONST = 1 << 1,
+    VIRTUAL = 1 << 2
+};
+public: CMethodUnit(const std::string & name,
+    const std::string & returnType, Flags
+  flags): m_name(name),
+m_returnType(returnType),
+m_flags(flags) {}
+void add(const std::shared_ptr < Unit > & unit, Flags /* flags */ = 0) {
+  m_body.push_back(unit);
+}
+std::string compile(unsigned int level = 0) const {
+  std::string result = generateShift(level);
+  if (m_flags & STATIC) {
+    result += "static ";
+  } else if (m_flags & VIRTUAL) {
+    result += "virtual ";
+  }
+  result += m_returnType + " ";
+  result += m_name + "()";
+  if (m_flags & CONST) {
+    result += " const";
+  }
+  result += " {\n";
+  for (const auto & b: m_body) {
+    result += b -> compile(level + 1);
+  }
+  result += generateShift(level) + "}\n";
+  return result;
+}
+private: std::string m_name;
+std::string m_returnType;
+Flags m_flags;
+std::vector < std::shared_ptr < Unit > > m_body;};
+class CClassUnit:public IClassUnit {  public: enum AccessModifier {
+        PUBLIC,
+        PROTECTED,
+        PRIVATE
+      };
+      static
+      const std::vector < std::string > ACCESS_MODIFIERS;
+      public: explicit CClassUnit(const std::string & name): m_name(name) {
+        m_fields.resize(ACCESS_MODIFIERS.size());
+      }
+      void add(const std::shared_ptr < Unit > & unit, Flags flags) {
+
+        int accessModifier = PRIVATE;
+
+        if (flags < ACCESS_MODIFIERS.size()) {
+          accessModifier = flags;
+        }
+        m_fields[accessModifier].push_back(unit);
+      }
+      std::string compile(unsigned int level = 0) const {
+        std::string result = generateShift(level) + "class " + m_name + " {\n";
+
+        for (size_t i = 0; i < ACCESS_MODIFIERS.size(); ++i) {
+          if (m_fields[i].empty()) {
+            continue;
+          }
+          result += ACCESS_MODIFIERS[i] + ":\n";
+          for (const auto & f: m_fields[i]) {
+            result += f -> compile(level + 1);
+          }
+          result += "\n";
+        }
+        result += generateShift(level) + "};\n";
+        return result;
+      }
+      private: std::string m_name;
+      using Fields = std::vector < std::shared_ptr < Unit > > ;
+      std::vector < Fields > m_fields;
+
+    };
+const std::vector< std::string > CClassUnit::ACCESS_MODIFIERS = { "public",
+"protected", "private" };
 
 
 
@@ -240,16 +313,21 @@ public:
 };
 
 class JavaFabric: public Fabric {
-
+    IClassUnit* createClass()override{
+    return new JavaClassUnit("kek");
+    }
+    IMethodUnit* createMethod() override{
+        return new JavaMethodUnit("foo","void",CsharpMethodUnit::STATIC);
+        }
 };
 class CFabric: public Fabric {
-
+    IClassUnit* createClass()override{
+    return new CClassUnit("kek");
+    }
+    IMethodUnit* createMethod() override{
+        return new CMethodUnit("foo","void",CsharpMethodUnit::STATIC);
+        }
 };
-
-
-
-
-
 class CsharpFabric: public Fabric {
     IClassUnit* createClass()override{
     return new CsharpClassUnit("kek");
@@ -259,8 +337,6 @@ class CsharpFabric: public Fabric {
         }
 
 };
-
-
 
 
 
